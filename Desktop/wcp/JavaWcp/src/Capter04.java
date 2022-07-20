92d365a1a05094762496ca787d4d404c54959075
USER <102310350+myhometony@users.noreply.github.com>
2022-07-20 15:12:33 +0900

    [Add]Capter04

---
+++ Desktop/wcp/JavaWcp/src/Capter04.java
@@ -0,0 +1,12 @@
+
+public class Capter04 {
+	public static void main(String[] args) {
+		String str1 = "山田太郎";
+		System.out.println(str1);
+		str1 = "いとをかし";
+		System.out.println(str1);
+		final int LUCKY_NUMBER = 2;
+		System.out.println(LUCKY_NUMBER);
+	}
+
+}
