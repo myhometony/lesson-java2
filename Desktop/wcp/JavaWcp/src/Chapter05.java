e9aeef8e0a9143d00bae4e107a69eddf3746132c
USER <102310350+myhometony@users.noreply.github.com>
2022-07-21 15:33:59 +0900

    [Add]Chapter06

---
+++ Desktop/wcp/JavaWcp/src/Chapter05.java
@@ -0,0 +1,20 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 1;
+		double double2 = 2;
+		boolean boolean3 = false;
+		System.out.println(long1);
+		System.out.println(double2);
+		System.out.println(boolean3);
+		Float float1 = 1F;
+		Integer integer2 = 2;
+		Character character3 = 'a';
+		System.out.println(float1);
+		System.out.println(integer2);
+		System.out.println(character3);
+		int[] int1 = { 0, 1, 2, 3, 4 };
+		System.out.println(int1[3]);
+	}
+
+}
